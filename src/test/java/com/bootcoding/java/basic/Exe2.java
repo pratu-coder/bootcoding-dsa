39c56525b23b1e237ba896731b4c3e629b084005
pratu-coder <wadgurepratiksha25@gmail.com>
2023-03-31 20:48:40 +0530

    array example

---
+++ src/test/java/com/bootcoding/java/basic/Exe2.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.basic;
+
+public class Exe2 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * n);
+        }
+    }
+}
