43c606032e05b5160fee9fe942c75189abf7737b
pratu-coder <wadgurepratiksha25@gmail.com>
2023-03-31 20:46:31 +0530

    array example

---
+++ src/test/java/com/bootcoding/java/basic/Example6.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.basic;
+
+public class Example6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 20; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
