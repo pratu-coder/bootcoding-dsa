7cc9cf339463b1354bd690a32e7f929a5b1bfc96
pratu-coder <wadgurepratiksha25@gmail.com>
2023-03-31 20:46:10 +0530

    array example

---
+++ src/test/java/com/bootcoding/java/basic/ArrayForLoop8.java
@@ -0,0 +1,18 @@
+package com.bootcoding.java.basic;
+
+public class ArrayForLoop8 {
+    public static void main(String[] args) {
+        int[] arr = {50, 10, 30, 40};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+
+
+            }
+
+        }
+        System.out.println("max ele of array is " + max);
+    }
+}
+
