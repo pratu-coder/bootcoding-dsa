1363e0bf20174d483e704077ad4adc3d095d867d
pratu-coder <wadgurepratiksha25@gmail.com>
2023-08-04 21:23:17 +0530

    StringEx1

---
+++ bootcoding-dsa/src/main/java/com/bootcoding/java/String/String1.java
@@ -0,0 +1,10 @@
+package main.java.com.bootcoding.java.String;
+
+public class String1 {
+    public static void main(String[] args){
+        String S1="Diwali";
+        String S2="diwali";
+        System.out.println(S1);
+        System.out.println(S2);
+    }
+}
