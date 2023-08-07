26091ed6bb70a3fec3962665220402036f4db12c
pratu-coder <wadgurepratiksha25@gmail.com>
2023-08-07 11:30:15 +0530

    StringEx4

---
+++ bootcoding-dsa/src/main/java/com/bootcoding/java/String/String4.java
@@ -0,0 +1,18 @@
+package main.java.com.bootcoding.java.String;
+
+public class String4 {
+    public static void main(String[] args) {
+        String s="rollno"+","+"cse"+","+"m1"+","+","+"m2"+","+"m3"+",";
+        StringBuilder sb=new StringBuilder();
+        sb.append("rollno");
+        sb.append(",");
+        sb.append("cse");
+        sb.append(",");
+        sb.append("m1");
+        sb.append(",");
+        sb.append("m2");
+        sb.append(",");
+
+
+    }
+}
