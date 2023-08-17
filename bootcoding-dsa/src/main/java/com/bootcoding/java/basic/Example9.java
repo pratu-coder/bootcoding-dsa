eddb6dd5fbc2b6f9cb24fe727133aefb2359a60d
pratu-coder <wadgurepratiksha25@gmail.com>
2023-08-17 22:56:05 +0530

    Example9

---
+++ bootcoding-dsa/src/main/java/com/bootcoding/java/basic/Example9.java
@@ -0,0 +1,12 @@
+package main.java.com.bootcoding.java.basic;
+
+public class Example9 {
+    public static void main(String[] args) {
+        int savings = 7000;
+        if (savings >= 5000) {
+            System.out.println("I will go with Neha");
+        } else {
+            System.out.println("I will not go with Neha");
+        }
+    }
+}
