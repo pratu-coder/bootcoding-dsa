49570e611dbbec7e7c7fc36694458c4407aff8a9
pratu-coder <wadgurepratiksha25@gmail.com>
2023-08-19 18:50:11 +0530

    Example11

---
+++ bootcoding-dsa/src/main/java/com/bootcoding/java/basic/Example11.java
@@ -0,0 +1,22 @@
+package main.java.com.bootcoding.java.basic;
+
+public class Example11 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+        int c = 50;
+
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a + "is the largest number");
+            } else {
+                System.out.println(c + "is the largest number");
+            }
+        } else {
+            if (b > c) {
+                System.out.println(b + "is the largest number");
+            }
+        }
+    }
+}
+
