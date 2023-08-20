33f33974832ea18dc52ca76cf8eea1ce49c4f7b2
pratu-coder <wadgurepratiksha25@gmail.com>
2023-08-20 22:27:06 +0530

    Example14

---
+++ bootcoding-dsa/src/main/java/com/bootcoding/java/basic/Example14.java
@@ -0,0 +1,15 @@
+package main.java.com.bootcoding.java.basic;
+
+public class Example14 {
+    public static void main(String[] args) {
+        int n = 30;
+        if (n > 20) {
+            System.out.println("more than 20");
+        } else if (n < 20) {
+            System.out.println("less than 20");
+        } else {
+            System.out.println("equal to 20");
+        }
+    }
+}
+
