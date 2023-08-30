5c8042f6bb757e4499abfbd13be0fff64027efd7
pratu-coder <wadgurepratiksha25@gmail.com>
2023-08-30 22:09:52 +0530

    Exe7

---
+++ bootcoding-dsa/src/main/java/com/bootcoding/java/basic/Exe7.java
@@ -0,0 +1,14 @@
+package main.java.com.bootcoding.java.basic;
+
+public class Exe7 {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
