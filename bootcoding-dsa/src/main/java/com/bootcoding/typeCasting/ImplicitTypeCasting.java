383829069cc4064c18b8f68d2e608d593913337d
pratu-coder <wadgurepratiksha25@gmail.com>
2023-09-04 18:35:05 +0530

    ImplicitTypeCasting

---
+++ bootcoding-dsa/src/main/java/com/bootcoding/typeCasting/ImplicitTypeCasting.java
@@ -0,0 +1,10 @@
+package main.java.com.bootcoding.typeCasting;
+
+public class ImplicitTypeCasting {
+    public static void main(String[] args) {
+        int a = 10; //4 bytes
+        double b = a; //8 bytes
+        System.out.println(b);
+
+    }
+}
